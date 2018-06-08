41a132231df6c0aec8ea348859efa7da47f57034
HeyGitHubb <yk942608890@gmail.com>
2018-06-08 18:45:47 +0800

    替换背景色

--- sample/src/main/java/com/dd/sample/CircularProgressButton.java
+++ sample/src/main/java/com/dd/sample/CircularProgressButton.java
@@ -2,8 +2,6 @@ package com.dd.sample;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.ColorStateList;
-import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
@@ -23,24 +21,18 @@ public class CircularProgressButton extends RelativeLayout {
 
     private GradientDrawable background;
 
-    private ColorStateList mIdleColorState;
-    private ColorStateList mCompleteColorState;
-    private ColorStateList mErrorColorState;
-
     private StateListDrawable mIdleStateDrawable;
     private StateListDrawable mCompleteStateDrawable;
-    private StateListDrawable mErrorStateDrawable;
 
     private StateManager mStateManager;
     private State mState;
 
-    private int mColorProgress;
     private float mCornerRadius;
     private boolean mIndeterminateProgressMode;
     private boolean mConfigurationChanged;
 
     private enum State {
-        PROGRESS, IDLE, COMPLETE, ERROR
+        PROGRESS, IDLE, COMPLETE
     }
 
     private int mMaxProgress;
@@ -50,30 +42,26 @@ public class CircularProgressButton extends RelativeLayout {
 
     public CircularProgressButton(Context context) {
         super(context);
-        init(context, null);
     }
 
     public CircularProgressButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
     }
 
     public CircularProgressButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init(context, attrs);
     }
 
     private View mViewContent;
 
     @Override protected void onFinishInflate() {
         super.onFinishInflate();
-        mViewContent = findViewById(R.id.root);
+        init();
     }
 
-    private void init(Context context, AttributeSet attributeSet) {
-        initAttributes(context, attributeSet);
+    private void init() {
+        mViewContent = findViewById(R.id.root);
         mCornerRadius = 90.0f;
-
         mMaxProgress = 100;
         mState = State.IDLE;
         mStateManager = new StateManager(this);
@@ -81,65 +69,22 @@ public class CircularProgressButton extends RelativeLayout {
         setBackgroundCompat(mIdleStateDrawable);
     }
 
-    private void initErrorStateDrawable() {
-        int colorPressed = getPressedColor(mErrorColorState);
-
-        GradientDrawable drawablePressed = createDrawable(colorPressed);
-        mErrorStateDrawable = new StateListDrawable();
-
-        mErrorStateDrawable.addState(new int[]{android.R.attr.state_pressed}, drawablePressed);
-        mErrorStateDrawable.addState(StateSet.WILD_CARD, background);
-    }
-
     private void initCompleteStateDrawable() {
-        int colorPressed = getPressedColor(mCompleteColorState);
-
-        GradientDrawable drawablePressed = createDrawable(colorPressed);
         mCompleteStateDrawable = new StateListDrawable();
-
-        mCompleteStateDrawable.addState(new int[]{android.R.attr.state_pressed}, drawablePressed);
         mCompleteStateDrawable.addState(StateSet.WILD_CARD, background);
     }
 
     private void initIdleStateDrawable() {
-        int colorNormal = getNormalColor(mIdleColorState);
-        int colorPressed = getPressedColor(mIdleColorState);
-        int colorFocused = getFocusedColor(mIdleColorState);
-        int colorDisabled = getDisabledColor(mIdleColorState);
         if (background == null) {
-            background = createDrawable(colorNormal);
+            background = createDrawable();
         }
-
-        GradientDrawable drawableDisabled = createDrawable(colorDisabled);
-        GradientDrawable drawableFocused = createDrawable(colorFocused);
-        GradientDrawable drawablePressed = createDrawable(colorPressed);
         mIdleStateDrawable = new StateListDrawable();
-
-        mIdleStateDrawable.addState(new int[]{android.R.attr.state_pressed}, drawablePressed);
-        mIdleStateDrawable.addState(new int[]{android.R.attr.state_focused}, drawableFocused);
-        mIdleStateDrawable.addState(new int[]{-android.R.attr.state_enabled}, drawableDisabled);
         mIdleStateDrawable.addState(StateSet.WILD_CARD, background);
     }
 
-    private int getNormalColor(ColorStateList colorStateList) {
-        return colorStateList.getColorForState(new int[]{android.R.attr.state_enabled}, 0);
-    }
-
-    private int getPressedColor(ColorStateList colorStateList) {
-        return colorStateList.getColorForState(new int[]{android.R.attr.state_pressed}, 0);
-    }
-
-    private int getFocusedColor(ColorStateList colorStateList) {
-        return colorStateList.getColorForState(new int[]{android.R.attr.state_focused}, 0);
-    }
-
-    private int getDisabledColor(ColorStateList colorStateList) {
-        return colorStateList.getColorForState(new int[]{-android.R.attr.state_enabled}, 0);
-    }
-
-    private GradientDrawable createDrawable(int color) {
+    private GradientDrawable createDrawable() {
         GradientDrawable drawable = (GradientDrawable) getResources().getDrawable(R.drawable.cpb_background).mutate();
-        drawable.setColor(Color.parseColor("#de686b"));
+        drawable.setColor(Color.parseColor("#a0de686b"));
         drawable.setCornerRadius(mCornerRadius);
         return drawable;
     }
@@ -152,9 +97,6 @@ public class CircularProgressButton extends RelativeLayout {
         } else if (mState == State.IDLE) {
             initIdleStateDrawable();
             setBackgroundCompat(mIdleStateDrawable);
-        } else if (mState == State.ERROR) {
-            initErrorStateDrawable();
-            setBackgroundCompat(mErrorStateDrawable);
         }
 
         if (mState != State.PROGRESS) {
@@ -162,45 +104,9 @@ public class CircularProgressButton extends RelativeLayout {
         }
     }
 
-    private void initAttributes(Context context, AttributeSet attributeSet) {
-        TypedArray attr = getTypedArray(context, attributeSet, R.styleable.CircularProgressButton);
-        if (attr == null) {
-            return;
-        }
-
-        try {
-            int white = getColor(R.color.cpb_white);
-
-            int idleStateSelector = attr.getResourceId(R.styleable.CircularProgressButton_cpb_selectorIdle,
-                    R.color.cpb_idle_state_selector);
-            mIdleColorState = getResources().getColorStateList(idleStateSelector);
-
-            int completeStateSelector = attr.getResourceId(R.styleable.CircularProgressButton_cpb_selectorComplete,
-                    R.color.cpb_complete_state_selector);
-            mCompleteColorState = getResources().getColorStateList(completeStateSelector);
-
-            int errorStateSelector = attr.getResourceId(R.styleable.CircularProgressButton_cpb_selectorError,
-                    R.color.cpb_error_state_selector);
-            mErrorColorState = getResources().getColorStateList(errorStateSelector);
-
-            mColorProgress = attr.getColor(R.styleable.CircularProgressButton_cpb_colorProgress, white);
-        } finally {
-            attr.recycle();
-        }
-    }
-
-    protected int getColor(int id) {
-        return getResources().getColor(id);
-    }
-
-    protected TypedArray getTypedArray(Context context, AttributeSet attributeSet, int[] attr) {
-        return context.obtainStyledAttributes(attributeSet, attr, 0, 0);
-    }
-
     public void setIndeterminateProgressMode(boolean indeterminateProgressMode) {
         this.mIndeterminateProgressMode = indeterminateProgressMode;
     }
-
 
     private MorphingAnimation createMorphing() {
         mMorphingInProgress = true;
@@ -216,9 +122,7 @@ public class CircularProgressButton extends RelativeLayout {
         } else {
             animation.setDuration(MorphingAnimation.DURATION_NORMAL);
         }
-
         mConfigurationChanged = false;
-
         return animation;
     }
 
@@ -246,8 +150,8 @@ public class CircularProgressButton extends RelativeLayout {
 
     private void morphToProgress() {
         MorphingAnimation animation = createProgressMorphing(mCornerRadius, getHeight(), getWidth(), getHeight());
-        animation.setFromColor(Color.parseColor("#de686b"));
-        animation.setToColor(Color.parseColor("#de686b"));
+        animation.setFromColor(Color.parseColor("#a0de686b"));
+        animation.setToColor(Color.parseColor("#a0de686b"));
         animation.setListener(mProgressStateListener);
         animation.start();
     }
@@ -264,16 +168,16 @@ public class CircularProgressButton extends RelativeLayout {
 
     private void morphProgressToComplete() {
         MorphingAnimation animation = createProgressMorphing(getHeight(), mCornerRadius, getHeight(), getWidth());
-        animation.setFromColor(mColorProgress);
-        animation.setToColor(getNormalColor(mCompleteColorState));
+        animation.setFromColor(Color.parseColor("#a0de686b"));
+        animation.setToColor(Color.parseColor("#a0de686b"));
         animation.setListener(mCompleteStateListener);
         animation.start();
     }
 
     private void morphIdleToComplete() {
         MorphingAnimation animation = createMorphing();
-        animation.setFromColor(getNormalColor(mIdleColorState));
-        animation.setToColor(getNormalColor(mCompleteColorState));
+        animation.setFromColor(Color.parseColor("#a0de686b"));
+        animation.setToColor(Color.parseColor("#a0de686b"));
         animation.setListener(mCompleteStateListener);
         animation.start();
     }
@@ -289,26 +193,10 @@ public class CircularProgressButton extends RelativeLayout {
 
     private void morphCompleteToIdle() {
         MorphingAnimation animation = createMorphing();
-
-        animation.setFromColor(getNormalColor(mCompleteColorState));
-        animation.setToColor(getNormalColor(mIdleColorState));
-
+        animation.setFromColor(Color.parseColor("#a0de686b"));
+        animation.setToColor(Color.parseColor("#a0de686b"));
         animation.setListener(mIdleStateListener);
-
         animation.start();
-
-    }
-
-    private void morphErrorToIdle() {
-        MorphingAnimation animation = createMorphing();
-
-        animation.setFromColor(getNormalColor(mErrorColorState));
-        animation.setToColor(getNormalColor(mIdleColorState));
-
-        animation.setListener(mIdleStateListener);
-
-        animation.start();
-
     }
 
     private OnAnimationEndListener mIdleStateListener = new OnAnimationEndListener() {
@@ -316,31 +204,23 @@ public class CircularProgressButton extends RelativeLayout {
         public void onAnimationEnd() {
             mMorphingInProgress = false;
             mState = State.IDLE;
-
             mStateManager.checkState(CircularProgressButton.this);
         }
     };
 
     private void morphIdleToError() {
         MorphingAnimation animation = createMorphing();
-
-        animation.setFromColor(getNormalColor(mIdleColorState));
-        animation.setToColor(getNormalColor(mErrorColorState));
-
+        animation.setFromColor(Color.parseColor("#a0de686b"));
+        animation.setToColor(Color.parseColor("#a0de686b"));
         animation.setListener(mErrorStateListener);
-
         animation.start();
-
     }
 
     private void morphProgressToError() {
         MorphingAnimation animation = createProgressMorphing(getHeight(), mCornerRadius, getHeight(), getWidth());
-
-        animation.setFromColor(mColorProgress);
-        animation.setToColor(getNormalColor(mErrorColorState));
-
+        animation.setFromColor(Color.parseColor("#a0de686b"));
+        animation.setToColor(Color.parseColor("#a0de686b"));
         animation.setListener(mErrorStateListener);
-
         animation.start();
     }
 
@@ -348,23 +228,19 @@ public class CircularProgressButton extends RelativeLayout {
         @Override
         public void onAnimationEnd() {
             mMorphingInProgress = false;
-            mState = State.ERROR;
-
             mStateManager.checkState(CircularProgressButton.this);
         }
     };
 
     private void morphProgressToIdle() {
         MorphingAnimation animation = createProgressMorphing(getHeight(), mCornerRadius, getHeight(), getWidth());
-
-        animation.setFromColor(mColorProgress);
-        animation.setToColor(getNormalColor(mIdleColorState));
+        animation.setFromColor(Color.parseColor("#a0de686b"));
+        animation.setToColor(Color.parseColor("#a0de686b"));
         animation.setListener(new OnAnimationEndListener() {
             @Override
             public void onAnimationEnd() {
                 mMorphingInProgress = false;
                 mState = State.IDLE;
-
                 mStateManager.checkState(CircularProgressButton.this);
             }
         });
@@ -418,8 +294,6 @@ public class CircularProgressButton extends RelativeLayout {
                 morphCompleteToIdle();
             } else if (mState == State.PROGRESS) {
                 morphProgressToIdle();
-            } else if (mState == State.ERROR) {
-                morphErrorToIdle();
             }
         }
     }
@@ -456,7 +330,6 @@ public class CircularProgressButton extends RelativeLayout {
             super.onRestoreInstanceState(state);
         }
     }
-
 
     static class SavedState extends BaseSavedState {
 
